a6cf5ca273d41674df0a9c599fa8e48a9cc238ba
Finley_ybc <51161205+Finley000ybc@users.noreply.github.com>
2024-11-04 22:45:04 +0800

    Update curl-api.sh

--- curl-api.sh
+++ curl-api.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-# 检查参数数量
 if [ "$#" -ne 3 ]; then
-    echo "使用方法: $0 <AKS_CLUSTER_NAME> <ACTION> <NAMESPACE>"
+    echo "use method: $0 <AKS_CLUSTER_NAME> <ACTION> <NAMESPACE>"
     exit 1
 fi
 
-# 位置变量
+# vars
 AKS_CLUSTER_NAME=$1
 ACTION=$2
 NAMESPACE=$3
 
-# 构建 URL
+# Build URL
 URL="https://${AKS_CLUSTER_NAME}-api.ap.test.com/int/rcs-test/${ACTION}"
 
-# 输出变量值
-echo "构建的 URL: $URL"
+echo "The Builded URL is: $URL"
 
-# 使用 curl 进行 POST 请求
+# Use curl POST
 RESPONSE=$(curl -s -X POST "$URL")
 
-# 输出结果到 txt 文件
+# Out put result to a txt file
 OUTPUT_FILE="${NAMESPACE}_response.txt"
 echo "$RESPONSE" > "$OUTPUT_FILE"
 
-# 输出结果文件的名称
-echo "返回结果已输出至: $OUTPUT_FILE"
+#
+echo "the result is in: $OUTPUT_FILE"
 
